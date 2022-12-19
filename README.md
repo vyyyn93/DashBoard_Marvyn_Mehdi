a087a095f23d55511f2f4989e070d9a22ccc70e3
vyyn93 <marvyn.martinez@icloud.com>
2022-12-19 12:13:29 +0100

    readme

--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 ## Introduction
 Ce projet s'effectue dans le cadre de l'unité E3FI-PR2 créé par Daniel Courivau. L'objectif de ce projet est d'utiliser des outils de base pour créer un DashBoard interactif sur un jeu de donnée de notre choix. Nous avons choisis de baser notre projet sur les statistiques des joueurs NBA. Le DashBoard contient un histogramme sur le nombre total de points marqués par les joueurs NBA et une representation géolocalisée des 20 meilleurs scoreurs de la ligue (point par match).
 
-Les données des joueurs ont été scrapper sur le site  [basketball reference](https://www.basketball-reference.com) dans l'onglet 'Leader' -> '2022-2023 NBA' -> 'Points'.
-Pour chaque joueur, 30 données statistiques différentes sont disponibles sur le site. Dans le processus de scrapping, nous récupérons ces 30 colonnes mais seul le nom, les points (PTS) et le nombre de matchs (G) joués sont utilisés dans le DashBoard.
+Les données des joueurs ont été récupérer sur le site  [basketball reference](https://www.basketball-reference.com) dans l'onglet 'Leader' -> '2022-2023 NBA' -> 'Points'.
+Pour chaque joueur, 30 données statistiques différentes par joueurs sont disponibles sur le site. Par exemple, nous avons acc_s à leur poste, leur équipe, le nombre de points marqué, le nombre de passe, leur pourcentage de reussite et leur nombre de match joué.
 
 ## User Guide
+### Istallation des modules et du dépôt GIT
 Pour utiliser le DashBoard sur votre machine, vous devez tout d'abord vous assurer d'avoir correctement installer Git. Pour installer les packages nécessaire à la bonne excecution du code, entrer la commande suivante dans un terminal:
 ``` 
 $ python -m pip install -r requirements.txt
 ```
 
-Pour cloner le depôt sur votre machine, entre la commande suivante dans un terminal:
+Pour cloner le depôt sur votre machine, entrez la commande suivante dans un terminal:
 ```
 $ cd Votre_Repertoire_De_Travail
 $ git clone adresse_publique_de_votre_projet
@@ -21,15 +22,21 @@ Pour lancer le scrapping des données et créer le DashBoard, excecutez cette co
 ```
 $ python main.py
 ```
+
 Vous pouvez consultez le DashBoard [ici](http://127.0.0.1:8050/) 
+
+### Utilisation du DashBoard
+Le DashBoard est composé de 2 pages distinctes. Pour passer d'une page à l'autre, cliquez sur le bouton 'Map' ou 'Histogramme'. La bouton de la page où vous vous trouvez change de couleur et deviens plus sombre.  
+
+Sur la page 'Histogramme', vous trouvez d'abord l'histogramme du nombre de point par match. Pour visualiser l'histogramme des passes par match, cliquez sur le bouton 'Assists Per Game' situé en dessous de l'histogramme.
+
+Sur la page 'Map', vous trouvez la géolocalisation des 10 meilleurs scoreurs NBA classé via la statistique Point par Match. Cliquez sur le PopUp rouge pour découvrir le joueur évoluant à cette endroit, son classement et son nombre de PPG.
+En dessous de la map, vous pourrez visualiser le classement en détail des 20 meilleurs scoreurs, ainsi que leur poste.
+
 ## Rapport d'analyse
-```mermaid
-graph TD
-    A[Christmas] -->|Get money| B(Go shopping)
-    B --> C{Let me think}
-    C -->|One| D[Laptop]
-    C -->|Two| E[iPhone]
-    C -->|Three| F[fa:fa-car Car]
-```
+Comme nous pouvons le voir sur l'histogramme des PPG, la grande majorité des joueurs marquent en moyenne entre 0 et 7 points par match. C'est encore plus flagrant pour les passes où plus de la moitié des joueurs effectue entre 0 et 1,5 passes par match. Ces graphiques montre l'hétérogénéité du niveau des joueurs NBA. En effet, seulement 0,08% ds joueurs NBA marque plus de 20 points par match. On voit donc l'héliocentrisme nautour des superstars, ce qui n'était pas autant le cas il y a 20 ans où la marque était mieux repartis entre les membres de l'équipe.  
+
+La map nous montre en repartition homogène des meilleurs joueurs. En effet, les meilleurs scoreurs sont équitablements repartis entre les deux conférences avec 5 joueurs à l'Eat, et 5 joueurs à l'Ouest.
+Cela montre un niveau homogène entre ces deux conférences, ce qui n'était pas aussi juste quelques années auparavent où la conférence Ouest regroupait plus de stars et où le niveau y  était bien plus relevé.
   
 ## Developper Guide
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Output, Input
 import webbrowser
 
@@ -38,8 +39,8 @@ def create_dataframe(url_site):
     for tr_balise in list_ligne_stat:
         list_data_player = tr_balise.find_all(['th', 'td'])
         list_stat = [td_balise.text for td_balise in list_data_player] # Liste des stats
-        df_length = len(dataframe)
-        dataframe.loc[df_length] = list_stat # Ajouter la liste à la DataFrame à l'index 'length'
+        dataframe_length = len(dataframe)
+        dataframe.loc[dataframe_length] = list_stat # Ajouter la liste à la DataFrame à l'index 'length'
         i+=1
     return dataframe
 
@@ -48,37 +49,54 @@ def stat_to_integer(dataframe):
     Convertis les statistiques numérique de la dataFrame en integer.
 
     Args:
-        df: DataFrame à convertir.
+        dataframe: DataFrame à convertir.
 
     Return:
-        df: DataFrame convertis.
+        dataframe: DataFrame convertis.
     """
     # Mets sous forme de list les statistiques à convertir (les 25 dernières)
-    list_stat = df.columns.tolist()[5::]
+    list_stat = dataframe.columns.tolist()[5::]
     for i in list_stat:
-        dataframe[i] = pd.to_numeric(df[i])
-    return df
+        dataframe[i] = pd.to_numeric(dataframe[i])
+    return dataframe
+
+def traitement_dataFrame(dataframe):
+    """Effectue les traitements suivants:
+        Ajout de la colonne PPG  
+
+        Ajoute de la colonne APP
+
+        Arrondis les valeurs à 2 chiffres significatifs
+
+        Trie les jouers par ordre croissant de PPG
+    
+    Args: dataFrame des joueurs NBA
+    
+    Return: Data frame fonctionnelle"""
+    dataframe["PPG"] = dataframe["PTS"]/dataframe["G"] # Ajoute de la colonne point par game au DataFrame
+    dataframe["APG"] = dataframe["AST"]/dataframe["G"]
+    dataframe = dataframe.round(2)
+    dataframe = dataframe.sort_values(by="PPG", ascending=False)
+    return dataframe
 #############################
 
 if __name__ == '__main__':
     #Scraping du site et création de la dataFrame
     URL = "https://www.basketball-reference.com/leagues/NBA_2023_totals.html#totals_stats::pts"
-    df = create_dataframe(URL)
-    df = stat_to_integer(df)
-    df["PPG"] = df["PTS"]/df["G"] # Ajoute de la colonne point par game au DataFrame
-    df["APG"] = df["AST"]/df["G"]
-    df = df.round(2)
-    df = df.sort_values(by="PPG", ascending=False)
+    dataframe = create_dataframe(URL)
+    dataframe = stat_to_integer(dataframe)
+    dataframe = traitement_dataFrame(dataframe)
     ###########################################
     # Définition des styles du DashBoard
     colors = {
-        'background': '#111111',
-        'text': '#7FDBFF'
+        'background': '#151516',
+        'text': '#0079D2'
     }
     H1_style = {
         'textAlign': 'center',
         'color': colors['text'],
         'marginTop':'20px',
+        'marginBottom':'20px',
         'font-family' : 'Trebuchet MS, sans-serif'
     }
     H3_style = {
@@ -88,26 +106,43 @@ if __name__ == '__main__':
         'font-family' : 'Trebuchet MS, sans-serif'
     }
     radio_button_style = {
-        'color':colors['text'],
-        'marginBottom':'50px',
-        'font-family' : 'Trebuchet MS, sans-serif'
+        'color':'#FFFFFF',
+        'font-family' : 'Trebuchet MS, sans-serif',
+        'fontSize' : '20px',
+        'padding':'30px'
     }
     tab_style={
         'backgroundColor':colors['background'],
         'color':colors['text'],
-        'font-family' : 'Trebuchet MS, sans-serif'
+        'font-family' : 'Trebuchet MS, sans-serif',
+        'text-align':'center',
+        'border-radius': '4px'
         }
+    tab_selected_style={
+    "background": "black",
+    'color': colors['text'],
+    'font-family' : 'Trebuchet MS, sans-serif',
+    'text-align':'center',
+    'align-items': 'center',
+    'justify-content': 'center',
+    'border-radius': '4px',
+    }
     map_style={
         'marginBottom':'30px'
     }
+    histo_style ={
+        'width':'1000px', 
+        "display": "block",
+        "margin-left": "auto",
+        "margin-right": "auto"
+    }
     TEMPLATE = 'plotly_dark'
     ####################################
-
     # Création des graphiques Plotly
-    data_tab = df[['Player', 'Pos', 'PPG']][0:20].transpose().values.tolist()
+    data_tab = dataframe[['Player', 'Pos', 'PPG']][0:20].transpose().values.tolist()
     data_tab.insert(0,[i for i in range(1, 21)])
 
-    lay=go.Layout(paper_bgcolor="#111",
+    lay=go.Layout(paper_bgcolor=colors['background'],
         margin={'t':0}
     )
 
@@ -123,29 +158,43 @@ if __name__ == '__main__':
     fig = go.Figure(data=[tableau],
                     layout=lay
             )
-    
 
     histoPPG = px.histogram(
-        df,
+        dataframe,
         x="PPG", nbins=25,
         labels={'PPG':'Points Per Game'},
         opacity=0.7,
         text_auto= True, 
-        template=TEMPLATE
+        template=TEMPLATE,
+        title = "Histogramme du nombre de point par match des "
+                + str(len(dataframe)) + " joueur NBA"
     )
 
     histoAPG = px.histogram(
-        df,
+        dataframe,
         x="APG", nbins=18,
         labels={'APG':'Assists Per Game'},
         opacity=0.7,
         text_auto= True,
-        template=TEMPLATE
+        template=TEMPLATE,
+        title = 'Histogramme du nombre de passe par match des '
+                                        + str(len(dataframe)) + ' joueur NBA'
     )
+    
+    radioItem = dcc.RadioItems(
+                    options={
+                        'PPG':'Points Per Game',
+                        'APG':'Assists Per Game'
+                    },
+                    value = 'PPG',
+                    inline=True,
+                    labelStyle=radio_button_style,
+                    id='radioButton'
+                )
     ####################################
 
     #Création du DashBoard
-    app = dash.Dash(__name__)
+    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
     app.layout = html.Div(
                     style={'backgroundColor':colors['background']},
@@ -160,27 +209,23 @@ if __name__ == '__main__':
                                 dcc.Tab(
                                     label='Histogamme',
                                     style=tab_style,
+                                    selected_style=tab_selected_style,
                                     children=[
-                                        html.H3(
-                                                style=H3_style,
-                                                id='title histo'
-                                            ),
-                                        dcc.Graph(id='graph1'),
-                                        dcc.RadioItems(
-                                            options={
-                                                'PPG':'Points Per Game',
-                                                'APG':'Assists Per Game'
-                                            },
-                                            value = 'PPG',
-                                            inline=True,
-                                            id='radioButton',
-                                            style=radio_button_style
-                                        )
+                                        html.Br(),
+                                        dcc.Graph(id='graph1', 
+                                        style=histo_style),
+                                        html.Br(),
+                                        html.Div(
+                                            radioItem,
+                                            style =  {'text-align':'center','display': 'block'},
+                                            className = "fs-5"),
+                                        html.Br()
                                     ]
                                 ),
                                 dcc.Tab(
                                     label='Map',
                                     style=tab_style,
+                                    selected_style=tab_selected_style, 
                                     children=[
                                         html.H3(
                                             children='Carte des 10 meillleur scoreurs NBA',
@@ -211,9 +256,9 @@ if __name__ == '__main__':
     ####################################
 
     #Méthode des callback
-    @app.callback([Output(component_id='graph1', component_property='figure'),
-                    Output(component_id='title histo', component_property='children')],
+    @app.callback(Output(component_id='graph1', component_property='figure'),
                   [Input(component_id= 'radioButton', component_property='value')])
+
     def update_histo(value):
         """
         Mets à jour l'histogramme en fonction du choix sur le radioButton
@@ -224,11 +269,9 @@ if __name__ == '__main__':
         Returns:
             Histogramme à afficher et son titre"""
         if value == 'PPG':
-            return [histoPPG, 'Histogramme du nombre de point par match des '
-                                        + str(len(df)) + ' joueur NBA']
+            return histoPPG
 
-        return [histoAPG,  'Histogramme du nombre d\'assists par match des '
-                                        + str(len(df)) + ' joueur NBA']
+        return histoAPG
     ####################################
 
     app.run_server(debug=True) # (8)
